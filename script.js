05cdb01f3ccb94856185b8730638e5296afe0e93
Nicholas Olds <nolds9@yahoo.com>
2015-07-14 19:34:52 -0400

    Added working question sequence

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 $(document).ready(function(){
     console.log("game ready")
     // game init
-    var questions = ["Jquery is a javascript library that prides itself on doing less with more","Javascript was created in 10 days in 1995","The vanilla JS selector to reference all p tags on a page is getElementsbyTagName('p')"]
-    var answers = ['false', 'true', 'true'];
-    $(".playerField").hide()
-    var score = parseInt($(".score").text());
-    console.log(score);
-    // event listeners
-    $("#c1").on("click", function(){
-        $("#gameBoard").hide();
-        $(".score").hide();
-        var points = parseInt($("#c1").text());
-        // console.log(points);
-        $(".question").append(questions[0])
-        $(".playerField").show()
-
-        $(".response").children().on("click", function(){
-          var result = $(this).html();
-          console.log(result);
-          if ( result == answers[0] ){
-            alert("correct!")
-          }
-
-
-        })
-
-    }); // end cell click event
+      $(".response").hide();
+      var questions = ["The 'Gang' consists of Charlie, Dee, Mac, Dennis, and Frank", "Kitten Mittons", "Charlie's favorite meal is milksteak","last question"]
+      var answers = ['true', 'false','true']
+      var selections = []
+      var index = 0;
+   // start game
+    $("#clickStart").on("click", function(){
+       $(".start").hide();
+       $(".questions").append(questions[index])
+       $(".response").show()
+    }) // end start event
+    // click event for true or false
+    $(".response").children().on("click", function(){
+       var result = $(this).html()
+       selections.push(result)
+       index ++
+       $(".questions").empty();
+       $(".questions").append(questions[index])
+    })
 })
